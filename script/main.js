c2b9b3599c23065e573a5120109987fd7d6a5b84
Fabio Gadau <fabiogadau92@gmail.com>
2020-04-15 15:31:19 +0200

    computerNumbers ok

--- script/main.js
+++ script/main.js
@@ -7,8 +7,59 @@
 
 $( document ).ready(function() {
   
+  var computerNumbers = [];
 
+  var maxNumber = 5;
 
+  var numbersRange = parseInt( prompt('Scegli difficoltà di gioco tra \n0 Facile \n1 Medio \n2 Difficile') );
+
+  while (isNaN(numbersRange) || numbersRange < 0 || numbersRange > 3) {
+    numbersRange == false;
+    numbersRange = parseInt( prompt('Scegli difficoltà di gioco tra \n0 Facile \n1 Medio \n2 Difficile') );
+  }
+  
+  /*if {
+    numbersRange == true;
+  }*/
+
+  switch(numbersRange) {
+    case 0:
+      maxNumber = 5;
+      break;
+    case 1:
+      maxNumber = 10;
+      break;
+    case 2:
+      maxNumber = 15;
+      break;
+    case 3:
+      maxNumber = 20;
+      break;
+  }
+
+  while (computerNumbers.length < maxNumber) {
+    
+    var numbers = randomNumbers(100)
+
+    if ( !computerNumbers.includes(numbers) ) {
+      computerNumbers.push(numbers);
+    }
+
+  };
+
+  console.log(computerNumbers);
+
+  
+  
+  
+  
+  
+  // Funzioni
+  function randomNumbers(max) {
+    
+    return Math.floor( Math.random() * max ) + 1;
+  
+  };
 
 
 
